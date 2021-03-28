63b32f104852758f247c6a1c0451da384f880376
YountenTshering <77493302+YountenTshering@users.noreply.github.com>
2021-03-28 13:09:50 +0700

    created packages and user.java

---
+++ AQMS/src/main/java/com/sdqi2021/AQMS/model/User.java
@@ -0,0 +1,32 @@
+package com.sdqi2021.AQMS.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean active;
+
+}
