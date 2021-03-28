8502193b9bf80a68958ca096dad434c804ccdace
YountenTshering <77493302+YountenTshering@users.noreply.github.com>
2021-03-28 11:08:57 +0700

    project created

---
+++ AQMS/src/main/java/com/sdqi2021/AQMS/AqmsApplication.java
@@ -0,0 +1,13 @@
+package com.sdqi2021.AQMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqmsApplication.class, args);
+	}
+
+}
