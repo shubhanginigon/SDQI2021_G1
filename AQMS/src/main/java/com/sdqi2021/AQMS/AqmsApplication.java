36ed9016f52f106e53d7c04685eed1753c97a4db
Suyogyart <suyogya.tamrakar@gmail.com>
2021-03-29 15:26:07 +0700

    Spring security and login design

--- AQMS/src/main/java/com/sdqi2021/AQMS/AqmsApplication.java
+++ AQMS/src/main/java/com/sdqi2021/AQMS/AqmsApplication.java
@@ -2,12 +2,26 @@ package com.sdqi2021.AQMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AqmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AqmsApplication.class, args);
+	}
+
+	@Configuration
+	public static class WebConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/**")
+					.addResourceLocations("classpath:/static/", "classpath:/images/")
+					.setCachePeriod(0);
+		}
 	}
 
 }
