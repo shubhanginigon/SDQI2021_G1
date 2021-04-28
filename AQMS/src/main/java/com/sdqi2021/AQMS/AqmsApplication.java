6ece78168c3c83c91b9cea959f55252667b51d68
Suyogyart <suyogya.tamrakar@gmail.com>
2021-04-28 19:29:40 +0700

    Solved dependency errors

--- AQMS/src/main/java/com/sdqi2021/AQMS/AqmsApplication.java
+++ AQMS/src/main/java/com/sdqi2021/AQMS/AqmsApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
